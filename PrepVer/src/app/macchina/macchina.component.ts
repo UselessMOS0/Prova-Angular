86b9ca9fb6afa2002f4fdc87591a3804f73f255b
UselessMOS0 <bono.walter@iisgalvanimi.edu.it>
2022-10-07 08:41:30 +0000

    7

--- PrepVer/src/app/macchina/macchina.component.ts
+++ PrepVer/src/app/macchina/macchina.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit ,Input} from '@angular/core';
 })
 export class MacchinaComponent implements OnInit {
   @Input() macchina:string = "";
+  madre_porta = ['viti','bulloni','ferro'] 
   constructor() {
    }
 
